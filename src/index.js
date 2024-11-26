06e7a354043961264098e9c032dfec1a77938765
LeonelCeballos18 <lceballos5@ucol.mx>
2024-11-25 22:13:04 -0800

    add crud author controller and routes, and postman collection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import prisma from "./prisma.js";
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import authorRoutes from "./routes/crud.author.routes.js"
 
 const app = express();
 const port = 3000;
 
 dotenv.config();
+
+app.use(bodyParser.json());
+
+app.use('/api/authors', authorRoutes);
+
 app.get("/", (req, res)=>{
     res.send("Hello world");
 })
@@ -15,6 +22,7 @@ app.listen(port, async()=>{
     console.log("\n---[Test db conection]---");
     try {
         await prisma.$connect();
+
         const author = await prisma.author.findFirst({
             orderBy: {
               author_id: 'asc',
